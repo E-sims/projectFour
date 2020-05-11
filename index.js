9afccbd510b9a7cb3c6fbf8743b363d8b1fc42d3
Eric Sims <50934788+E-sims@users.noreply.github.com>
2020-05-11 17:56:58 -0500

    feat: add popup form functionality

---
+++ index.js
@@ -0,0 +1,24 @@
+let editButton = document.querySelector('.profile__edit-btn');
+let closeButton = document.querySelector('.popup__close');
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__input_type_name');
+let jobInput = formElement.querySelector('.form__input_type_job');
+let userName = document.querySelector('.profile__name');
+let userJob = document.querySelector('.profile__about');
+
+let togglePopup = () => {
+  popup.classList.toggle('popup_opened');
+}
+
+editButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
+
+formElement.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  userName.textContent = nameInput.value;
+  userJob.textContent = jobInput.value;
+
+  togglePopup();
+})
