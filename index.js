ee7aa728057804247805a2927713788a04429590
Eric Sims <50934788+E-sims@users.noreply.github.com>
2020-05-12 17:13:48 -0500

    fix: index.js refactor

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let editButton = document.querySelector('.profile__edit-btn');
-let closeButton = document.querySelector('.popup__close');
-let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('.form__input_type_name');
-let jobInput = formElement.querySelector('.form__input_type_job');
-let userName = document.querySelector('.profile__name');
-let userJob = document.querySelector('.profile__about');
+const editButton = document.querySelector('.profile__edit-btn');
+const closeButton = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
+const formElement = document.querySelector('.form');
+const nameInput = formElement.querySelector('.form__input_type_name');
+const jobInput = formElement.querySelector('.form__input_type_job');
+const userName = document.querySelector('.profile__name');
+const userJob = document.querySelector('.profile__about');
 
-let togglePopup = () => {
+const togglePopup = () => {
   popup.classList.toggle('popup_opened');
 }
 
-let closePopup = () => {
+const closePopup = () => {
   nameInput.value = userName.textContent;
   jobInput.value = userJob.textContent;
   popup.classList.toggle('popup_opened');
