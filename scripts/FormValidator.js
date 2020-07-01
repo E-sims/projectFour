c0213e6c0fe984209f1a391fa922434d13dda1df
Eric Sims <50934788+E-sims@users.noreply.github.com>
2020-07-01 18:15:05 -0500

    feat: add modules folder

---
+++ scripts/FormValidator.js
@@ -0,0 +1,11 @@
+export class FormValidator {
+  constructor(config, formElement) {
+    this._formElement = formElement;
+
+    this._inputSelector = config.inputSelector;
+    this._submitButtonSelector = config.submitButtonSelector;
+    this._inactiveButtonClass = config.inactiveButtonClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._errorClass = config.errorClass;
+  }
+}
